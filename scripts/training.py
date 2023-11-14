45af5d502f5f647575452216869d8ec23d7fce4f
Jan Kokla <jan.kokla@gmail.com>
2023-11-14 11:48:01 +0100

    add training func

--- scripts/training.py
+++ scripts/training.py
@@ -1,8 +1,12 @@
 """training.py: helper functions for convenient training."""
+import random
+
+import numpy as np
 import segmentation_models_pytorch as smp
+import torch
 
 
-def train_epoch(model, device, dataloader, criterion, optimizer):
+def _train_epoch(model, device, dataloader, criterion, optimizer) -> (float, float):
     """
     Train the model and return epoch loss and average f1 score.
 
@@ -39,12 +43,10 @@ def train_epoch(model, device, dataloader, criterion, optimizer):
     average_loss = running_loss / len(dataloader)
     average_f1 = running_f1 / len(dataloader)
 
-    print(f'loss: {average_loss:.3f}, f1: {average_loss:.3f}')
-
     return average_loss, average_f1
 
 
-def valid_epoch(model, device, dataloader, criterion):
+def _valid_epoch(model, device, dataloader, criterion) -> (float, float):
     """
     Validate the model performance by calculating epoch loss and average f1 score.
 
@@ -77,3 +79,41 @@ def valid_epoch(model, device, dataloader, criterion):
     average_f1 = running_f1 / len(dataloader)
 
     return average_loss, average_f1
+
+
+def setup_seed(seed: int, cuda: bool = False):
+    """
+    Create global seed for torch, numpy and cuda.
+
+    :param seed:
+    :param cuda: boolean whether to use gpu
+    """
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+def train_model(model, criterion, optimizer, dataloaders, num_epochs):
+    train_loader, valid_loader = dataloaders
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.to(device)
+
+    train_losses, valid_losses, train_f1s, valid_f1s = [], [], [], []
+
+    for epoch in range(num_epochs):
+
+        train_loss, train_f1 = _train_epoch(model, device, train_loader, criterion, optimizer)
+        valid_loss, val_f1 = _valid_epoch(model, device, valid_loader, criterion)
+
+        train_losses.append(train_loss)
+        valid_losses.append(valid_loss)
+        train_f1s.append(train_f1)
+        valid_f1s.append(val_f1)
+
+        print('{} Loss: {:.4f} Acc: {:.4f}'.format(epoch, valid_loss, val_f1))
+
+    return train_losses, valid_losses, train_f1s, valid_f1s
